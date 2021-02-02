f465055d10369e585fcf88eaff0ae2f9b6617f4a
lunaymira <73600388+lunaymira@users.noreply.github.com>
2021-02-02 21:23:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 "use strict"
 
-$(document).ready(function () {
-	$('a[href*=#]').bind("click", function (e) {
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-		e.preventDefault();
-	});
-	return false;
-});
-
 
 document.addEventListener('DOMContentLoaded', function () {
 	const form = document.getElementById('form');
